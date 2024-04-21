6108b4cce2ed375121daa6ffd172018169766a00
Simon <82274800+Lecho67@users.noreply.github.com>
2024-04-21 16:54:45 -0500

    Modificando Cosas

--- src/main/java/com/Polizas/Polizas/PolizasApplication.java
+++ src/main/java/com/Polizas/Polizas/PolizasApplication.java
@@ -19,11 +19,11 @@ public class PolizasApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PolizasApplication.class, args);
-		RedisService redisService = new RedisService();
-		Transaccion transaccion = new Transaccion(1L,10.6,"USD");
-		redisService.saveTransaccion(3L,transaccion);
-		Transaccion transaccion1 = redisService.getTransaccion(3L);
-		System.out.println(transaccion1.toString());
+//		RedisService redisService = new RedisService();
+//		Transaccion transaccion = new Transaccion(1L,10.6,"USD");
+//		redisService.saveTransaccion(3L,transaccion);
+//		Transaccion transaccion1 = redisService.getTransaccion(3L);
+//		System.out.println(transaccion1.toString());
 
 	}
 
