6ccc5904424457f7befbec045a959a342487bc67
Simon <82274800+Lecho67@users.noreply.github.com>
2024-05-09 17:22:28 -0500

    puta

--- src/test/java/com/Polizas/Polizas/PolizasApplicationTests.java
+++ src/test/java/com/Polizas/Polizas/PolizasApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class PolizasApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+//	@Test
+//	void contextLoads() {
+//	}
 
 }
